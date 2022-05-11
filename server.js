800293757ac93e69d7f3d97453a2cdcd65a3df0e
Shrimp <lcamara0916@gmail.com>
2022-05-11 12:04:59 +0000

    Fixed typo

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.listen(12345, () => {
-    console.log("Server on port 6000")
+app.listen(5000, () => {
+    console.log("Server on port 5000")
 });
 
 app.use(express.static(__dirname + '/public/'));
