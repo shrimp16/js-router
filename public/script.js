cfa3920cc2a2fdfef5204db0656b107a5ba0fdf5
Shrimp <lcamara0916@gmail.com>
2022-05-11 12:01:31 +0000

    Made implementation of navigation with buttons

--- public/script.js
+++ public/script.js
@@ -3,3 +3,11 @@ import Router from './pages/router.js';
 document.body.onload = () => {
     new Router();
 }
+
+document.getElementById('go-to-login').addEventListener('click', () => {
+    window.location.href = '/#login';
+})
+
+document.getElementById('go-to-register').addEventListener('click', () => {
+    window.location.href = '/#register';
+})
